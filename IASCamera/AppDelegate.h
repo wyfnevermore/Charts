f7a724f6a59df59d34942b34127e099003b9343e
wyfnevermore <843645610@qq.com>
2017-04-01 09:10:07 +0800

    Initial Commit

---
+++ IASCamera/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  IASCamera
+//
+//  Created by wyfnevermore on 2017/4/1.
+//  Copyright © 2017年 wyfnevermore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ IASCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IASCamera
+//
+//  Created by wyfnevermore on 2017/4/1.
+//  Copyright © 2017年 wyfnevermore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
